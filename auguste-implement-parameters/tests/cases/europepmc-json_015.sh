b3d1230c1c3f6acdf5a1964b8cee0d6d7a250789
saulius <saulius@63c7f2d9-9d11-409a-a4db-444dd1c7f1db>
2017-08-16 11:23:32 +0000

    branches/feature/auguste-implement-parameters/ (saulius@koala.ibt.lt) Implementing the '--xml-output' option in the 'europepmc-json' script; adding a test case.
    git-svn-id: svn://saulius-grazulis.lt/bibliophile/branches/feature@103 63c7f2d9-9d11-409a-a4db-444dd1c7f1db

---
+++ auguste-implement-parameters/tests/cases/europepmc-json_015.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+set -ue
+
+./europepmc-json \
+    --search IND601311758+OR+IND601311566+OR+IND601309362+OR+IND601309237 \
+    --xml-output \
+| perl -lne 'print $1 while /<id>(.*?)<\/id>/g' \
+| sort
